d440c7381f13d5ee3cea1a8aa6de609d4378cf93
rj-devnation <rgeli90@gmail.com>
2024-01-11 22:27:01 -0600

    Created the navbar layout.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+
+    return (
+        <>
+            <div className="navbar">
+                <div className="greeting">
+                    <li>Welcome, User!</li>
+                </div>
+                <div className="nav-links">
+                    <li>Dashboard</li>
+                    <li>Tickets</li>
+                    <li>Reports</li>
+                    <li>User Profile</li>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
