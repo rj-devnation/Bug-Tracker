575d1e005a174aca02ede9d0ecc9814de71da323
rj-devnation <rgeli90@gmail.com>
2024-01-14 03:37:43 -0600

    change: browserrouter to hashrouter for project to work on github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 /** Components */
 import Layout from './components/Navigation/Layout'
@@ -12,23 +12,32 @@ import Dashboard from './pages/Dashboard'
 import Login from './pages/Login/Login'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
 
   const handleLogin = () => {
     setIsLoggedIn(true);
+
   }
+
+  // let navigate = useNavigate()
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     navigate("/dashboard")
+  //   }, [])
+  // }, handleLogin)
 
   return (
     <>
       {/* {!isLoggedIn && <Layout><Dashboard /></Layout>} */}
       {!isLoggedIn && 
       <Routes>
-        <Route index path='/login' element={<Login onLogin={handleLogin} />} />
+        <Route path='/' element={<Login onLogin={handleLogin} />} />
       </Routes>}
       {isLoggedIn && (
         <Layout>
           <Routes>
-            <Route index path='/dashboard' element={<Dashboard />} />
+            <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/projects' element={<Projects />} />
             <Route path='/tickets' element={<Tickets />} />
             <Route path='/admin' element={<Admin />} />
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import './index.css'
 import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter basename='/Bug-Tracker/'>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
+  <HashRouter>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+  </HashRouter>
 )
